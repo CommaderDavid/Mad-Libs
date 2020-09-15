8ed9b2119ac8e61f86f7076544ff45a24c7e1339
David Boedigheimer <stealth259@gmail.com>
2020-09-15 16:00:23 -0700

    Add all routes for Controller.

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -5,6 +5,17 @@ namespace MadLibs.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("/")]
+        public string Hello() { return "Hello friend! Welcome to Mad Libs!"; }
 
+        [Route("/form")]
+        public ActionResult Form() { return View(); }
+
+        [Route("/lib")]
+        public ActionResult Lib()
+        {
+            LibVariable myLibVariable = new LibVariable(name, verb1, verb2, verb3, noun1, noun2, noun3, adj1, adj2, adj3);
+            return View(myLibVariable);
+        }
     }
 }
