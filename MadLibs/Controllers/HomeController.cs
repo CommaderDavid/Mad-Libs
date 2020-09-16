d814346772c0f7c6ac20201b6adc8427d9695647
David Boedigheimer <stealth259@gmail.com>
2020-09-16 13:58:04 -0700

    Add fix to form and controller.

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MadLibs.Controllers
         public ActionResult Form() { return View(); }
 
         [Route("/lib")]
-        public ActionResult Lib()
+        public ActionResult Lib(string name, string verb1, string verb2, string verb3, string noun1, string noun2, string noun3, string adj1, string adj2, string adj3)
         {
             LibVariable myLibVariable = new LibVariable(name, verb1, verb2, verb3, noun1, noun2, noun3, adj1, adj2, adj3);
             return View(myLibVariable);
